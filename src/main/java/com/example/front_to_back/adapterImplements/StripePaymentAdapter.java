105b6079bddff5af9ba71df489e8013fbd9491bf
noir <rion123tea@gmail.com>
2023-10-27 15:09:06 +0700

    что-то не работает

--- src/main/java/com/example/front_to_back/adapterImplements/StripePaymentAdapter.java
+++ src/main/java/com/example/front_to_back/adapterImplements/StripePaymentAdapter.java
@@ -16,11 +16,14 @@ public class StripePaymentAdapter implements PaymentAdapter {
         // Stripe API
         try {
             // Создаем параметры для сессии платежа
+            String YOUR_DOMAIN = "http://localhost:4242";
+            String SUCCESS_URL = YOUR_DOMAIN+"/resources/public/success.html";
+            String CANCEL_URL = YOUR_DOMAIN+"/resources/public/cancel.html";
             //TODO: разобраться со скидкой
             SessionCreateParams params = SessionCreateParams.builder()
                     .setMode(SessionCreateParams.Mode.PAYMENT)
-                    .setSuccessUrl("https://example.com/success.html")  // Замените на свой URL успешной оплаты
-                    .setCancelUrl("https://example.com/cancel.html")    // Замените на свой URL отмены оплаты
+                    .setSuccessUrl(SUCCESS_URL)  // Замените на свой URL успешной оплаты
+                    .setCancelUrl(CANCEL_URL)    // Замените на свой URL отмены оплаты
                     .addLineItem(
                             SessionCreateParams.LineItem.builder()
                                     .setQuantity(1L)
--- src/main/java/org/example/mySubTask/Payment.java
+++
@@ -1,7 +0,0 @@
-package org.example.mySubTask;
-
-import java.io.UnsupportedEncodingException;
-
-public interface Payment {
-    void processPayment(String wallet_id, double amount, String recipient, String message) throws UnsupportedEncodingException;
-}
