0cd3401a4f874321447998b67c73265440cf414a
noir <rion123tea@gmail.com>
2023-11-11 08:39:29 +0700

    добавил модель заказа и интерфейс обработчика

---
+++ demo/src/main/java/com/example/demo/handlers/OrderHandler.java
@@ -0,0 +1,7 @@
+package com.example.demo.handlers;
+
+import com.example.demo.models.Order;
+
+public interface OrderHandler {
+    boolean handle(Order o);
+}
---
+++ demo/src/main/java/com/example/demo/models/Order.java
@@ -0,0 +1,7 @@
+package com.example.demo.models;
+
+public record Order(
+        /**
+         * перечисляем необходимые поля..
+         */
+) { }
