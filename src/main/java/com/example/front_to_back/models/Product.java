012baebc0461a4670cd2aac18caf441bc1ebe13f
noir <rion123tea@gmail.com>
2023-11-11 07:44:44 +0700

    начинаем с чистого листа

--- src/main/java/com/example/front_to_back/models/Product.java
+++
@@ -1,7 +0,0 @@
-package com.example.front_to_back.models;
-
-//public class Product {
-////    private List<CartItem> items; // Список товаров в корзине
-//}
-// TODO: подстряпать id продукта со страйпа сюда
-public record Product(String id, Object info){ }
